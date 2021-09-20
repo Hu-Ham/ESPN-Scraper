ea4ee0ff0aeb424841fc1ccc2013b676ae7aaab4
Lucius Latham <kingcodefish@gmail.com>
2021-09-20 13:09:09 -0400

    Add get article links

--- main.py
+++ main.py
@@ -1,20 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
-import re
 
-URL = input("Enter a URL to scrape for pizza: ")
-page = requests.get(URL)
+page = requests.get("https://www.espn.com/")
 
 soup = BeautifulSoup(page.content, "html.parser")
-title = soup.find('title').getText()
-results = soup.find_all('p')
-count = 0
-for i in results:
-    if "pizza" in i.getText().lower():
-        count += 1
-links = soup.find_all('a', attrs={'href': re.compile("^http://")})
-print("Title: " + title)
-print("Number of times 'pizza' occurs: " + str(count))
-print("All web links:")
-for i in links:
-    print(i.get('href'))
+for a in soup.find_all('a', class_='contentItem__padding'):
+    subpage = requests.get(a['href'])
