99ec3b29f5412a4e0133830583903c41a5176682
Lucius Latham <kingcodefish@gmail.com>
2021-09-21 19:46:19 -0400

    Scrape top 10 articles and get mean and median words, will need to be redone for top 20.

--- main.py
+++ main.py
@@ -1,8 +1,33 @@
 import requests
 from bs4 import BeautifulSoup
+from collections import Counter
+from string import punctuation
+import statistics
+import nltk
 
 page = requests.get("https://www.espn.com/")
 
 soup = BeautifulSoup(page.content, "html.parser")
-for a in soup.find_all('a', class_='contentItem__padding'):
-    subpage = requests.get(a['href'])
+headlinesDiv = soup.find('div', {'class': 'headlineStack'})
+
+# Follow top headline to get full list of top headlines.
+link = "https://www.espn.com" + headlinesDiv.find('a', {'data-mptype': 'headline'})['href']
+storyPage = requests.get(link)
+storyPageSoup = BeautifulSoup(storyPage.content, "html.parser")
+
+totalWords = []
+
+for a in storyPageSoup.find_all('a', {'class': 'story-link'}):
+    subpageLink = "https://www.espn.com" + a['href']
+    subpage = requests.get(subpageLink)
+    subpageSoup = BeautifulSoup(subpage.content, "html.parser")
+    articleBody = subpageSoup.find('div', {'class': 'article-body'})
+
+    text_p = (''.join(s.findAll(text=True)) for s in articleBody.findAll('p'))
+    c_p = Counter((x.rstrip(punctuation).lower() for y in text_p for x in y.split()))
+
+    print("Title: " + subpageSoup.find('title').getText())
+    totalWords.append(len(c_p.values()))
+
+print("Mean Words: " + str(statistics.mean(totalWords)))
+print("Median Words: " + str(statistics.median(totalWords)))
